1096453fa342e55455c6e6ffee59297208de4364
Francois Baud <fxbaud@gmail.com>
2020-01-23 20:03:37 +0000

    with controller, routes and index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating Flats...'
+100.times do
+  flats_attributes = [
+  {
+    name:         Faker::FunnyName.unique.two_word_name,
+    address:      Faker::Address.full_address,
+    description:  Faker::Lorem.unique.paragraph_by_chars(number: 240, supplemental: false),
+    price_per_nights: rand(55..350),
+    number_of_guests: rand(2..15)
+  }
+]
+Flat.create!(flats_attributes)
+end
+
+puts 'Finished!'
